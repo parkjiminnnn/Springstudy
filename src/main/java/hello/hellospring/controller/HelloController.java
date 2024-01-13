ca959657c63a5a1335d0bdc9a29657d5ad1b0ee0
parkjiminnnn <pjm5489@naver.com>
2024-01-13 23:15:06 +0900

    api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +21,30 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+    @GetMapping("hello-string")
+    @ResponseBody       //http의 body에 문자 내용을 직접 반환
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
